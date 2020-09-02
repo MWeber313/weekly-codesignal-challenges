68e04f1a746a23d8362918ce3ac6b6a6a4a950fa
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-09-01 23:02:03 -0700

    Added skeleton of css grid

--- css-grid/src/App.js
+++ css-grid/src/App.js
@@ -7,6 +7,31 @@ function App() {
       <header className="App-header">
         <h1>This is my calculator</h1>
         <div class="container">
+          <h2>Calculator</h2?>
+          <div class="calculator">
+            {/*This is the input window, clear key and division key */}
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            {/*This is the first row (7-9 plux mult) of keys*/}
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            {/*This is the second row (4-6 plus subtract) of keys */}
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            {/*This is the third row (1-3 plus addition) of keys */}
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+            {/*This is the zero key and the sum key */}
+            <div class="calc-input"><p></p></div>
+            <div class="calc-input"><p></p></div>
+          </div>
         </div>
       </header>
     </div>
